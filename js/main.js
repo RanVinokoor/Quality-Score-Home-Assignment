94d3314d583bbd6dde0fb12db8a5bfa7851c48c6
Ran Vinokoor <78797448+RanVinokoor@users.noreply.github.com>
2022-10-12 16:33:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@
 var subscribeModeBtn = document.getElementById("subscribe-mode");
 var purchaseModeBtn = document.getElementById("purchase-mode");
 var disclaimerBox = document.getElementById("disclaimer");
+var productStamp = document.getElementById("product-stamp");
 var checkboxBtn = document.getElementById("checkbox");
 var ctaBtn = document.getElementById("cta-btn");
 var onePackageBtn = document.getElementById("one-package");
 var threePackageBtn = document.getElementById("three-package");
 var sixPackageBtn = document.getElementById("six-package");
+var productImage = document.getElementById("product-image");
 var priceMain = document.getElementById("price-main");
 var priceRetail = document.getElementById("price-retail");
 var priceSavings = document.getElementById("price-savings");
+
+// Click events
+onePackageBtn.addEventListener("click", () => {
+  onePackage();
+});
+
+threePackageBtn.addEventListener("click", threePackage);
+sixPackageBtn.addEventListener("click", sixPackage);
+purchaseModeBtn.addEventListener("click", purchaseMode);
+subscribeModeBtn.addEventListener("click", subscribeMode);
+
+checkboxBtn.addEventListener("click", () => {
+  validate(checkboxBtn, ctaBtn);
+});
 
 // Package prices
 var prices = {
@@ -23,6 +39,10 @@ var prices = {
   sixPackagePriceRetail: 348.00,
   sixPackagePriceSubscribe: 167.40
 };
+
+// Default product & stamp images
+productImage.src = "./img/3pack.png";
+productStamp.src = "./img/stamp.png";
 
 // Default prices
 priceMain.value = "$" + Math.round(prices.threePackagePriceMain);
@@ -51,8 +71,8 @@ function purchaseMode() {
   purchaseModeBtn.classList.add("active");
   disclaimerBox.style.display = "none";
   checkboxBtn.checked = false;
-  ctaBtn.style.pointerEvents ="unset";
-  ctaBtn.style.backgroundColor ="#7fb457";
+  ctaBtn.classList.add('cta-enabled');
+  ctaBtn.classList.remove('cta-disabled');
 
   if (onePackageBtn.classList.contains("active") && purchaseModeBtn.classList.contains("active")) {
     priceMain.value = "$" + Math.round(prices.onePackagePriceMain);
@@ -81,8 +101,8 @@ function subscribeMode() {
   purchaseModeBtn.classList.remove("active");
   subscribeModeBtn.classList.add("active");
   disclaimerBox.style.display = "block";
-  ctaBtn.style.pointerEvents ="none";
-  ctaBtn.style.backgroundColor ="#9a9b9c";
+  ctaBtn.classList.add('cta-disabled');
+  ctaBtn.classList.remove('cta-enabled');
 
   if (onePackageBtn.classList.contains("active") && subscribeModeBtn.classList.contains("active")) {
     priceMain.value = "$" + Math.round(prices.onePackagePriceSubscribe) + "/month";
@@ -108,13 +128,13 @@ function subscribeMode() {
 }
 
 // Checkbox validation
-function validate(){
-  if (checkboxBtn.checked){
-    ctaBtn.style.pointerEvents ="unset";
-    ctaBtn.style.backgroundColor ="#7fb457";
+function validate(x, y){
+  if (x.checked){
+    y.classList.add('cta-enabled');
+    y.classList.remove('cta-disabled');
   } else {
-    ctaBtn.style.pointerEvents ="none";
-    ctaBtn.style.backgroundColor ="#9a9b9c";
+    y.classList.add('cta-disabled');
+    y.classList.remove('cta-enabled');
   }
 }
 
@@ -124,69 +144,111 @@ ctaBtn.onclick = function() {
 
 // Package options
 function onePackage() {
+  productImage.src = "./img/1product.png";
+  productStamp.src = "./img/stamp.png";
+  productStamp.classList.add("display-none")
+
   threePackageBtn.classList.remove("active");
   sixPackageBtn.classList.remove("active");
   onePackageBtn.classList.add("active");
 
-  purchaseModeBtn.classList.add("active");
-  subscribeModeBtn.classList.remove("active");
   disclaimerBox.style.display = "none";
-  ctaBtn.style.pointerEvents ="unset";
-  ctaBtn.style.backgroundColor ="#7fb457";
+  ctaBtn.classList.add('cta-enabled');
+  ctaBtn.classList.remove('cta-disabled');
   checkboxBtn.checked = false;
-
-  priceMain.value = "$" + Math.round(prices.onePackagePriceMain);
-  priceRetail.value = "$" + Math.round(prices.onePackagePriceRetail);
-  priceSavings.value = "$" + (Math.round(prices.onePackagePriceRetail) - Math.round(prices.onePackagePriceMain));
 
   ctaBtn.onclick = function() {
     window.open('https://www.tracker.com/link-1', '_blank');
+  }
+
+  if (onePackageBtn.classList.contains("active") && subscribeModeBtn.classList.contains("active")) {
+    disclaimerBox.style.display = "block";
+    ctaBtn.classList.add('cta-disabled');
+    ctaBtn.classList.remove('cta-enabled');
+    priceMain.value = "$" + Math.round(prices.onePackagePriceSubscribe) + "/month";
+    priceRetail.value = "$" + Math.round(prices.onePackagePriceRetail);
+    priceSavings.value = "$" + (Math.round(prices.onePackagePriceRetail) - Math.round(prices.onePackagePriceSubscribe));
+    ctaBtn.onclick = function() {
+      window.open('https://www.tracker.com/subscribe-link-1', '_blank');
+    }
+  } else {
+    priceMain.value = "$" + Math.round(prices.onePackagePriceMain);
+    priceRetail.value = "$" + Math.round(prices.onePackagePriceRetail);
+    priceSavings.value = "$" + (Math.round(prices.onePackagePriceRetail) - Math.round(prices.onePackagePriceMain));
   }
 
   inputWidth();
 }
 
 function threePackage() {
+  productImage.src = "./img/3pack.png";
+  productStamp.src = "./img/stamp.png";
+  productStamp.classList.remove("display-none")
+
   onePackageBtn.classList.remove("active");
   sixPackageBtn.classList.remove("active");
   threePackageBtn.classList.add("active");
 
-  purchaseModeBtn.classList.add("active");
-  subscribeModeBtn.classList.remove("active");
   disclaimerBox.style.display = "none";
-  ctaBtn.style.pointerEvents ="unset";
-  ctaBtn.style.backgroundColor ="#7fb457";
+  ctaBtn.classList.add('cta-enabled');
+  ctaBtn.classList.remove('cta-disabled');
   checkboxBtn.checked = false;
-
-  priceMain.value = "$" + Math.round(prices.threePackagePriceMain);
-  priceRetail.value = "$" + Math.round(prices.threePackagePriceRetail);
-  priceSavings.value = "$" + (Math.round(prices.threePackagePriceRetail) - Math.round(prices.threePackagePriceMain));
 
   ctaBtn.onclick = function() {
     window.open('https://www.tracker.com/link-3', '_blank');
+  }
+
+  if (threePackageBtn.classList.contains("active") && subscribeModeBtn.classList.contains("active")) {
+    disclaimerBox.style.display = "block";
+    ctaBtn.classList.add('cta-disabled');
+    ctaBtn.classList.remove('cta-enabled');
+    priceMain.value = "$" + Math.round(prices.threePackagePriceSubscribe) + "/month";
+    priceRetail.value = "$" + Math.round(prices.threePackagePriceRetail);
+    priceSavings.value = "$" + (Math.round(prices.threePackagePriceRetail) - Math.round(prices.threePackagePriceSubscribe));
+    ctaBtn.onclick = function() {
+      window.open('https://www.tracker.com/subscribe-link-3', '_blank');
+    }
+  } else {
+    priceMain.value = "$" + Math.round(prices.threePackagePriceMain);
+    priceRetail.value = "$" + Math.round(prices.threePackagePriceRetail);
+    priceSavings.value = "$" + (Math.round(prices.threePackagePriceRetail) - Math.round(prices.threePackagePriceMain));
   }
 
   inputWidth();
 }
 
 function sixPackage() {
+  productImage.src = "./img/6pack.png";
+  productStamp.src = "./img/stamp_green.png";
+  productStamp.classList.remove("display-none")
+
   onePackageBtn.classList.remove("active");
   threePackageBtn.classList.remove("active");
   sixPackageBtn.classList.add("active");
 
-  purchaseModeBtn.classList.add("active");
-  subscribeModeBtn.classList.remove("active");
   disclaimerBox.style.display = "none";
-  ctaBtn.style.pointerEvents ="unset";
-  ctaBtn.style.backgroundColor ="#7fb457";
+  ctaBtn.classList.add('cta-enabled');
+  ctaBtn.classList.remove('cta-disabled');
   checkboxBtn.checked = false;
-
-  priceMain.value = "$" + Math.round(prices.sixPackagePriceMain);
-  priceRetail.value = "$" + Math.round(prices.sixPackagePriceRetail);
-  priceSavings.value = "$" + (Math.round(prices.sixPackagePriceRetail) - Math.round(prices.sixPackagePriceMain));
 
   ctaBtn.onclick = function() {
     window.open('https://www.tracker.com/link-6', '_blank');
+  }
+
+  if (sixPackageBtn.classList.contains("active") && subscribeModeBtn.classList.contains("active")) {
+    disclaimerBox.style.display = "block";
+    ctaBtn.classList.add('cta-disabled');
+    ctaBtn.classList.remove('cta-enabled');
+    priceMain.value = "$" + Math.round(prices.sixPackagePriceSubscribe) + "/month";
+    priceRetail.value = "$" + Math.round(prices.sixPackagePriceRetail);
+    priceSavings.value = "$" + (Math.round(prices.sixPackagePriceRetail) - Math.round(prices.sixPackagePriceSubscribe));
+    ctaBtn.onclick = function() {
+      window.open('https://www.tracker.com/subscribe-link-6', '_blank');
+    }
+  } else {
+    priceMain.value = "$" + Math.round(prices.sixPackagePriceMain);
+    priceRetail.value = "$" + Math.round(prices.sixPackagePriceRetail);
+    priceSavings.value = "$" + (Math.round(prices.sixPackagePriceRetail) - Math.round(prices.sixPackagePriceMain));
   }
 
   inputWidth();
